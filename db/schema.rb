3cd0587eaece4210ea4c900620c1b5679465a5f8
ro <kronew6@gmail.com>
2022-01-17 15:19:44 -0600

    removes author column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_17_211248) do
+ActiveRecord::Schema.define(version: 2022_01_17_211632) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,7 +21,6 @@ ActiveRecord::Schema.define(version: 2022_01_17_211248) do
     t.string "isbn"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "author"
   end
 
 end
